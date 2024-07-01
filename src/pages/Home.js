23ad3cb9364c4a18424ea2209411fe1f4076cc55
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-07-01 07:09:16 +0545

    react

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
-const App = () => {
-
-
-
+const Home = () => {
   return (
     <div>
 
@@ -12,4 +9,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Home
