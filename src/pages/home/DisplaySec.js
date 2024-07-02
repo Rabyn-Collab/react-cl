55818f70505be1940452867afd9d440203636caf
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-07-02 07:46:11 +0545

    ch

--- src/pages/home/DisplaySec.js
+++ src/pages/home/DisplaySec.js
@@ -2,16 +2,21 @@ import React from 'react'
 
 const DisplaySec = () => {
   return (
-    <div>
+    <div className='grid grid-cols-2 items-center'>
 
-      <div className="dis-img">
+      <div className="dis-img h-[470px]">
+
+        <dotlottie-player src="https://lottie.host/27af2812-b66e-470e-aa2b-a691e6b34886/cozAQH3gRT.json" background="transparent" speed="1" loop autoplay></dotlottie-player>
+
 
       </div>
 
 
 
-      <div className="dis-info">
-
+      <div className="dis-info space-y-3">
+        <h1 className='text-3xl font-bold'>Hi, I am John </h1>
+        <p className='text-pink-700 italic'>Some Dev, Freelancer, Rounder</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quos optio incidunt. Ab sapiente dolorem nam aut a deleniti quas magnam! Doloribus debitis modi quod fuga dicta reprehenderit, cum laborum dolor sapiente sed delectus aspernatur eius sint itaque rerum aut perspiciatis rem quisquam, voluptatibus labore mollitia sit. Eaque, explicabo quam!</p>
       </div>
 
 
