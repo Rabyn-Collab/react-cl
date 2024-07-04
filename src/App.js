f62fdae8852243314c397de5ce5f4792efc420f3
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-07-04 15:31:47 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React, { useState } from 'react'
-import Header from './components/Header'
-import DisplaySec from './pages/home/DisplaySec'
-import Tech from './pages/home/Tech'
-import Intro from './pages/home/Intro'
-import Footer from './components/Footer'
+import React from 'react'
 
 const App = () => {
-
-
-
   return (
     <div>
-
-
-
-
-      <Header />
-      <DisplaySec />
-      <Tech />
-      <Intro />
-      <Footer />
 
     </div>
   )
