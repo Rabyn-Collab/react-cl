8deae5b213970d0f20bb6b792da0beb5ed02cb53
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-07-29 15:24:27 +0545

    naya kura ho

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,34 @@
-import CategoryCard from '../components/CategoryCard';
-import { useApiHooks } from '../hooks/apiHooks';
+
+import React, { useMemo, useState } from 'react'
 
 const HomePage = () => {
+  const [incre, setIncre] = useState(0);
+  const [decre, setDecre] = useState(100);
 
-  const [load, data, err] = useApiHooks('https://www.themealdb.com/api/json/v1/1/categories.php', {});
+  const dependIncre = useMemo(() => {
+    for (let i = 0; i < 1000000000; i++) {
 
-  if (load) {
-    return <h1>Loading....</h1>
-  }
+    }
+    console.log('function run');
+    return 2 * incre;
+  }, [incre]);
 
-  if (err) {
-    return <h1>{err}</h1>
-  }
+  // console.log(dependIncre);
 
 
 
   return (
-    <div className='p-4 grid grid-cols-3 gap-4'>
+    <div>
 
+      <h1>{dependIncre}</h1>
+      <button onClick={() => setIncre((prev) => prev + 1)}>Increment Garne</button>
 
-      {data && data?.categories.map((cata) => {
-        return <CategoryCard
-          cata={cata}
-          key={cata.idCategory
-          } />;
+      <p>--------------------------------------------</p>
 
-      })}
+      <h1>Decrement ko part {decre}</h1>
+      <h1>{decre}</h1>
+
+      <button onClick={() => setDecre((prev) => prev - 1)}>Decrement Garne</button>
 
 
 
@@ -35,3 +38,54 @@ const HomePage = () => {
 }
 
 export default HomePage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import CategoryCard from '../components/CategoryCard';
+// import { useApiHooks } from '../hooks/apiHooks';
+
+// const HomePage = () => {
+
+//   const [load, data, err] = useApiHooks('https://www.themealdb.com/api/json/v1/1/categories.php', {});
+
+//   if (load) {
+//     return <h1>Loading....</h1>
+//   }
+
+//   if (err) {
+//     return <h1>{err}</h1>
+//   }
+
+
+
+//   return (
+//     <div className='p-4 grid grid-cols-3 gap-4'>
+
+
+//       {data && data?.categories.map((cata) => {
+//         return <CategoryCard
+//           cata={cata}
+//           key={cata.idCategory
+//           } />;
+
+//       })}
+
+
+
+
+//     </div>
+//   )
+// }
+
+// export default HomePage
